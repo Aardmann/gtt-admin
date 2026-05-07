a9127e778949995d34d534f6f57ac18b8a416968
Aardmann <nketiaaardman@gmail.com>
2026-05-07 21:20:46 +0000

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   reactCompiler: true,
+  basePath: '/ghana-trotro-admin',
+  assetPrefix: '/ghana-trotro-admin/',
 };
 
 export default nextConfig;
